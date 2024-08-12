b377ee8e17f2278a52665837746af276a671d3f1
zongweizong <love0927774486@gmail.com>
2024-08-12 20:32:38 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -169,52 +169,49 @@ def handle_message(event):
 
      ################################股票提醒################################
 
-    if re.match('關閉提醒',msg):
-        import schedule
-        schedule.clear()
-    if re.match('提醒股票',msg):
+    if re.match("股價提醒", msg):
         import schedule
         import time
-        #查看當前股價
-        def look_stock_price(stock,condition,price,userID):
+        # 查看當前股價
+        def look_stock_price(stock, condition, price, userID):
             print(userID)
             url = 'https://tw.stock.yahoo.com/q/q?s=' + stock
             list_req = requests.get(url)
-            soup = BeautifulSoup(list_req.content,"html.parser")
-            getstock = soup.find('span',class_='Fz(32px)').string
-            content = stock +"當前股市價格為" + getstock
+            soup = BeautifulSoup(list_req.content, "html.parser")
+            getstock = soup.find('span', class_='Fz(32px)').string
+            content = stock + "當前股市價格為: " +  getstock
             if condition == '<':
-                content += '\n 篩選條件為 < ' + price
+                content += "\n篩選條件為: < "+ price
                 if float(getstock) < float(price):
-                    content += "\n符合" + getstock + " < " + price + '的篩選條件'
-                    line_bot_api.push_message(userID, TextSendMessage(text = content))
+                    content += "\n符合" + getstock + " < " + price + "的篩選條件"
+                    line_bot_api.push_message(userID, TextSendMessage(text=content))
             elif condition == '>':
-                content += '\n篩選條件為 >' + price
+                content += "\n篩選條件為: > "+ price
                 if float(getstock) > float(price):
-                    content += "\n符合" + getstock + " > " + price + '的篩選條件'
-                    line_bot_api.push_message(userID, TextSendMessage(text = content))
-            elif condition == '=':
-                content += '\n篩選條件為 =' + price
+                    content += "\n符合" + getstock + " > " + price + "的篩選條件"
+                    line_bot_api.push_message(userID, TextSendMessage(text=content))
+            elif condition == "=":
+                content += "\n篩選條件為: = "+ price
                 if float(getstock) == float(price):
-                    content += "\n符合" + getstock + " = " + price + '的篩選條件'
-                    line_bot_api.push_message(userID, TextSendMessage(text = content))
-        # look_stock_price(stock = '2002',condition = '>',price = 31)
+                    content += "\n符合" + getstock + " = " + price + "的篩選條件"
+                    line_bot_api.push_message(userID, TextSendMessage(text=content))
+        # look_stock_price(stock='2002', condition='>', price=31)
         def job():
             print('HH')
             dataList = cache_users_stock()
-            #print(dataList)
+            # print(dataList)
             for i in range(len(dataList)):
                 for k in range(len(dataList[i])):
-                    #print(dataList[i])
-                    look_stock_price(dataList[i][k]['favorite_stock'],dataList[i][k]['condition'],dataList[i][k]['price'],dataList[i][k]['userID'])
-
-        schedule.every(10).seconds.do(job).tag('daily-tasks-stock'+uid,'second')#每10秒執行一次
-        #
-        #
-        #
-        #
-        #無窮迴圈
-        while True:
+                    # print(dataList[i][k])
+                    look_stock_price(dataList[i][k]['favorite_stock'], dataList[i][k]['condition'], dataList[i][k]['price'], dataList[i][k]['userID'])
+                    # look_stock_price(stock='2002', condition='>', price=31)
+        schedule.every(10).seconds.do(job).tag('daily-tasks-stock'+uid,'second') #每10秒執行一次
+        #schedule.every().hour.do(job) #每小時執行一次
+        #schedule.every().day.at("17:19").do(job) #每天9點30執行一次
+        #schedule.every().monday.do(job) #每週一執行一次
+        #schedule.every().wednesday.at("14:45").do(job) #每週三14點45執行一次
+        # 無窮迴圈
+        while True: 
             schedule.run_pending()
             time.sleep(1)
     #新增使用者關注的股票到mongodb EX:關注2330>XXX
