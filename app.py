5ff0f8ee55dde6d7bc15aa1c9449ab0987e15d55
zongweizong <love0927774486@gmail.com>
2024-08-21 22:46:53 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -224,8 +224,8 @@ def handle_message(event):
         line_bot_api.push_message(uid, content)
         return 0
     if re.match("分析趨勢圖",msg):
-        messages = Msg_Template.stock_reply_other()
-        line_bot_api.push_message(event.reply_token,messages)
+        message = Msg_Template.stock_reply_other()
+        line_bot_api.reply_message(event.reply_token,message)
     
     ############################### 股票區 ################################
     
